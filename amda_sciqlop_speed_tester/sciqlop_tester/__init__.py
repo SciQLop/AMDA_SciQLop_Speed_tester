9f9823bcd63acc126911f7fa6bb77be86c1621d7
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-10-15 17:37:53 +0200

    Remove needs of spwc
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- amda_sciqlop_speed_tester/sciqlop_tester/__init__.py
+++ amda_sciqlop_speed_tester/sciqlop_tester/__init__.py
@@ -3,13 +3,14 @@ from datetime import datetime
 import pickle
 
 
+
 def get_from_proxy(product='thb_bl', start_time='2018-10-14%2000:00:00', stop_time='2018-10-14%2006:00:00',server="http://sciqlop.lpp.polytechnique.fr/cache"):
     if type(start_time) is datetime:
         start_time = start_time.isoformat()
     if type(stop_time) is datetime:
         stop_time = stop_time.isoformat()
     resp=requests.get(f"{server}/get_data?path=amda/{product}&start_time={start_time}&stop_time={stop_time}")
-    return pickle.loads(resp.content)
+    return resp.content
 
 
 def dl_from_rest_api():

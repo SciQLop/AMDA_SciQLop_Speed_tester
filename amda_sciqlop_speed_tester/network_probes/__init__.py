b7f8eec5614771d20a58c9a9a29f0e29601cb40d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-10-17 19:03:32 +0200

    Made traceroute faster on OSX and Linux
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- amda_sciqlop_speed_tester/network_probes/__init__.py
+++ amda_sciqlop_speed_tester/network_probes/__init__.py
@@ -6,6 +6,5 @@ def trace_route(address: str):
     if platform.system() == 'Windows':
         route = subprocess.run(['tracert', address], stdout=subprocess.PIPE).stdout.decode('cp850')
     else:
-        route = subprocess.run(['traceroute', address], stdout=subprocess.PIPE).stdout.decode()
+        route = subprocess.run(['traceroute', '-w1', '-m15', address], stdout=subprocess.PIPE).stdout.decode()
     return route
-
